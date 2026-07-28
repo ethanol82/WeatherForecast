30d771961765a8d6af58671d817250fb47aafebd
ethan <shinuzan0832@gmail.com>
2026-07-28 16:20:26 +0900

    first commit

---
+++ WeatherForecast/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  WeatherForecast
+//
+//  Created by WooChan Shin on 7/28/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            DayForecast(day: "Mon", isRainy: false, high: 70, low: 50)
+            
+            DayForecast(day: "Tue", isRainy: true, high: 60, low: 40)
+        }
+    }
+}
+struct DayForecast: View {
+    let day: String
+    let isRainy: Bool
+    let high: Int
+    let low: Int
+    
+    var iconName: String {
+        isRainy ? "cloud.rain.fill" : "sun.max.fill"
+    }
+    
+    var iconColor: Color {
+        isRainy ? Color.blue : Color.yellow
+    }
+    
+    var body: some View {
+        VStack {
+            Text(day)
+                .font(Font.headline)
+            Image(systemName: iconName)
+                .foregroundStyle(iconColor)
+                .font(Font.largeTitle)
+                .padding(5)
+            Text("High: \(high)º")
+                .fontWeight(Font.Weight.semibold)
+            Text("Low: \(low)º")
+                .fontWeight(Font.Weight.medium)
+                .foregroundStyle(Color.secondary)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by WooChan Shin on 7/28/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
